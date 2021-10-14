3b1db3facab27d7fb0a11df0cb339dd38b7dd502
mybabygrand <amuro_ray@kakao.com>
2021-10-15 00:12:02 +0900

    Section8. Proxy and Management of Relation, start

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -33,27 +35,51 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class,  member2.getId());
+//            Member findMember = em.find(Member.class,  member2.getId());
 //            System.out.println(findMember.toString());
 //            System.out.println(findMember.getTeam().toString());
 
-            System.out.println(findMember.getTeam()+"'s MemberList");
-            for(Member m : findMember.getTeam().getMembers()){
-                System.out.println(m.toString());
-            }
+//            System.out.println(findMember.getTeam()+"'s MemberList");
+//            for(Member m : findMember.getTeam().getMembers()){
+//                System.out.println(m.toString());
+//            }
 
             //수정  A->B팀
 //            findMember.enrollOrChangeTeam(team2);
 
 
-            Locker locker = new Locker();
-            locker.setName("A Locker");
-            locker.setMember(member2);
-            em.persist(locker);
+//            Locker locker = new Locker();
+//            locker.setName("A Locker");
+//            locker.setMember(member2);
+//            em.persist(locker);
+            //Case0. proxy only
+            Member refMember = em.getReference(Member.class,  3L);
+            System.out.println("refMember.getClass() : "+ refMember.getClass());
+            System.out.println("isLoaded : "+ emf.getPersistenceUnitUtil().isLoaded(refMember));//초기화 확인
+//            refMember.getUsername();//강제 초기화
+            Hibernate.initialize(refMember);//강제 초기화
+            System.out.println("isLoaded : "+ emf.getPersistenceUnitUtil().isLoaded(refMember));//초기화 확인
+            System.out.println("======");
+
+            //Case1. proxy & real
+            Member referenceMember = em.getReference(Member.class,  member.getId());
+            System.out.println("referenceMember.getClass() : "+referenceMember.getClass());
+            Member findMember = em.find(Member.class,  member.getId());// 프록시를 통해 실제에 접근함(어째뜬  프록시임)
+            System.out.println("findMember.getClass()) : "+findMember.getClass());
+            System.out.println(referenceMember.getClass() == findMember.getClass());
+            System.out.println("======");
+
+            //Case2. real & proxy
+            Member findMember2 = em.find(Member.class,  member2.getId());//실제에 접근함
+            System.out.println("findMember2.getClass() : "+findMember2.getClass());
+            Member referenceMember2 = em.getReference(Member.class,  member2.getId());//실제에 이미 가져왔기에(영속성 컨텍스트에 있으므로) 굳이 proxy를 안씀
+            System.out.println("referenceMember2.getClass() : "+referenceMember2.getClass());
+            System.out.println(referenceMember2.getClass() == findMember2.getClass());
 
             tx.commit();
         }catch (Exception e){
            tx.rollback();
+           e.printStackTrace();
         }finally {
             em.close();
         }
@@ -61,3 +87,4 @@ public class JpaMain {
 
     }
 }
+
